5e7f8f001e5b6e6885e94f1244593c8d57fcb4dc
Angele SIONNEAU <a.sionneau@hotmail.fr>
2022-04-13 17:28:09 +0200

    implement Shader and use it in Asset

--- 3DPart/Assets.h
+++ 3DPart/Assets.h
@@ -2,13 +2,15 @@
 #include <map>
 #include <string>
 #include "Texture.h"
+#include "Shader.h"
 using std::map;
 using std::string;
 
 class Assets
 {
 public:
-	static std::map<std::string, Texture> textures;
+	static map<string, Texture> textures;
+	static map<string, Shader> shaders;
 
 	//Loads a texture from file
 	static Texture LoadTexture(IRenderer& renderer, const string& filename, const string& name);
@@ -19,10 +21,24 @@ public:
 	//Properly de-allocates all loaded resources
 	static void Clear();
 
+	//loads and generate a shader from file loading (shader's source code)
+	static Shader LoadShader(const string& vShaderFile, const string& fShaderFile,
+		const string& tcShaderFile, const string& teShaderFile,
+		const string& gShaderFile, const string& name);
+
+	//retrieve a stored Shader
+	static Shader& GetShader(const string& name);
+	
+
 private:
 	Assets(){}
 
 	//Loads a single texture from file
 	static Texture LoadTextureFromFile(IRenderer& renderer, const string& filename);
+
+	//load and generate a shader from file
+	static Shader LoadShaderFromFile(const string& vShaderFile, const string& fShaderFile,
+		const string& tcShaderFile = "", const string& teShaderFile = "",
+		const string& gShaderFile = "");
 };
 
